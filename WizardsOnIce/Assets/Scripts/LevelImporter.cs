b5c5bcae73b2afd76161f5701add32e991bc0ca6
anoldlady9 <DCHENG5@depaul.edu>
2017-02-24 15:38:58 -0600

    Added new spikes, new levels, and new build

--- WizardsOnIce/Assets/Scripts/LevelImporter.cs
+++ WizardsOnIce/Assets/Scripts/LevelImporter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if (UNITY_EDITOR)
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -106,3 +107,4 @@ public class LevelImporter : EditorWindow {
 	
 	}
 }
+#endif
