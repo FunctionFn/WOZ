61c807445e76fe8075e84052c2f281238ac8143e
FunctionFn <daniel.fernow@gmail.com>
2017-04-09 19:41:32 -0500

    Made cdtext follow the player

--- WizardsOnIce/Assets/Scripts/PlayerController.cs
+++ WizardsOnIce/Assets/Scripts/PlayerController.cs
@@ -187,11 +187,6 @@ public class PlayerController : MonoBehaviour
 			
         }
 
-        if(PlayerNumber == "0" && currentMaxSpeed > maxSpeed)
-        {
-            Debug.Log(currentMaxSpeed);
-        }
-
         rb.angularVelocity = Vector3.zero;
 
         ControlUpdate();
@@ -247,12 +242,8 @@ public class PlayerController : MonoBehaviour
         if(AbilityTimer <= 0)
         {
             cdtext.enabled = false;
+            
         }
-
-        
-        
-        
-        
 
         if (GrabTimer <= 0)
         {
@@ -287,6 +278,8 @@ public class PlayerController : MonoBehaviour
             Kill();
 
         }
+
+        CooldownPositionUpdate();
 
     }
 
@@ -621,5 +614,26 @@ public class PlayerController : MonoBehaviour
         }
     }
    
+    public void CooldownPositionUpdate()
+    {
+        //GameObject WorldObject;
+
+        //this is the ui element
+        //RectTransform UI_Element;
+
+        //first you need the RectTransform component of your canvas
+        RectTransform CanvasRect = cdtext.canvas.GetComponent<RectTransform>();
+
+        //then you calculate the position of the UI element
+        //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+
+        Vector2 ViewportPosition = mainCamera.GetComponent<Camera>().WorldToViewportPoint(transform.position);
+        Vector2 WorldObject_ScreenPosition = new Vector2(
+        ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+        ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+        //now you can set the position of the ui element
+        cdtext.GetComponent<RectTransform>().anchoredPosition = WorldObject_ScreenPosition;
+    }
 }
 
